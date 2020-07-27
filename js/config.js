cb7e1a5b40f9f6628c95bce74f37d87a296dd444
edwardv <edventuranza@yahoo.com>
2020-07-27 10:27:42 -0400

    Updated js/config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_DUHv5pfYE', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '64i17v9s94muom58ujvm2ps2tm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
